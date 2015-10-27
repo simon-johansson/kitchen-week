923e73f501b3542c585ad3dd63ce87d513123cc6
Simon Johansson <mail@simon-johansson.com>
2015-10-27 23:40:31 +0100

    Add some styling to the templates

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,10 @@ const spreadsheet = require('../lib/spreadsheet');
 const propagate = require('../lib/propagate');
 const router = express.Router();
 
-// router.post('/poke', function(req, res) {
-//   var addresses = req.body.emails.join(', ');
-
-//   mail.send(addresses, function (error, info) {
-//     if(error){
-//       return res.status(500).json({ error: 'Could not send email' });
-//     }
-//     console.log('Sent reminders to', addresses);
-//     res.json('Emails sent!');
-//   });
-// });
-
 router.get('/', (req, res, next) => {
   spreadsheet.getData((err, data) => {
     if (err) {
-      var error = new Error(err);
+      let error = new Error(err);
       error.status = 500;
       return next(error);
     }
--- routes/mail.js
+++ routes/mail.js
@@ -1,25 +1,55 @@
 
+const async = require('async');
 const express = require('express');
 const router = express.Router();
 const spreadsheet = require('../lib/spreadsheet');
 const propagate = require('../lib/propagate');
+const gif = require('../lib/gif');
 const {
   reminderTemplate,
   statusTemplate,
   summaryTemplate
 } = require('../lib/mail/templates');
 
-const respond = (req, res, next, template) => {
-  spreadsheet.getData((err, data) => {
+const getData = (template, callback) => {
+  async.parallel({
+    spreadsheet: clb => {
+      spreadsheet.getData((err, data) => clb(err, data));
+    },
+    positiveImage: clb => {
+      gif.findPositive((err, data) => clb(err, data));
+    },
+    negativeImage: clb => {
+      gif.findNegative((err, data) => clb(err, data));
+    }
+  }, (err, results) => {
     if (err) {
-      var error = new Error(err);
+      return callback(err);
+    }
+
+    const data = results.spreadsheet;
+    data.images = {
+      positive: results.positiveImage,
+      negative: results.negativeImage,
+    };
+
+    // console.log(data);
+
+    template.render(data)
+      .then( compile => {
+        callback(null, compile);
+      });
+  });
+};
+
+const respond = (req, res, next, template) => {
+  getData(template, (err, data) => {
+    if (err) {
+      let error = new Error(err);
       error.status = 500;
       return next(error);
     }
-    template.render(data)
-      .then( results => {
-        res.send(results.html);
-      });
+    res.send(data.html);
   });
 };
 
