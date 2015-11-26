2ad99a962d82cfde9d1b0896044e2fb768d5f1f8
Simon Johansson <mail@simon-johansson.com>
2015-11-26 22:51:56 +0100

    Change variable name PORT to port
    Hopefully makes a difference for windows azure

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./schedule');
 // Initiate and start server
 var app = require('./app');
 
-app.set('port', process.env.PORT || 22334);
+app.set('port', process.env.port || 22334);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
