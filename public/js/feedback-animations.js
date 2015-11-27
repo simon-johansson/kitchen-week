50d833158ffb28a4b9357b6b4ae58272503f431a
Simon Johansson <mail@simon-johansson.com>
2015-11-27 22:47:45 +0100

    Fixes and optimisations to feedback animations

--- public/js/feedback-animations.js
+++ public/js/feedback-animations.js
@@ -6,46 +6,28 @@ const $body = $('body');
 let counter = random(0, 2);
 
 const brad = () => {
-  const numberOfBrads = 1;
-
-  for (let ii = numberOfBrads; ii >= 1; ii--) {
-    // const size = `${random(500, 550)}px`;
-    const size = '700px';
-    const el = $('<div/>')
-      .addClass('brad')
-      .css({
-        // 'left': `${random(0, 50)}%`,
-        'left': '15%',
-        'width': size,
-        'height': size,
-        'margin-left': `-${size / 2}px`,
-        // 'top': `-${size}px`,
-        // '-webkit-transition-duration': `${random(15, 20)}s`,
-        '-webkit-transition-duration': '20s',
-        // '-webkit-transition-delay': `${random(0.0, 4.0)}s`,
-      });
-    $body.prepend(el);
-  };
-
-  setTimeout(() => $('.brad').addClass('drop'), 10);
-  setTimeout(() => $('.brad').remove(), 22000);
+  $('.brad')
+    .addClass('drop')
+    .one('webkitTransitionEnd otransitionend oTransitionEnd transitionend',
+      function (e) {
+        console.log('remove');
+        $(this).removeClass('drop');
+    });
 };
 
 const walking = () => {
-  const el = $('<div/>').addClass('walking');
-  $body.prepend(el);
-
-  setTimeout(() => $('.walking').addClass('walk'), 10);
-  setTimeout(() => $('.walking').remove(), 15000);
+  $('.walking')
+    .addClass('walk')
+    .one('webkitTransitionEnd otransitionend oTransitionEnd transitionend',
+      function (e) {
+        console.log('remove');
+        $(this).removeClass('walk');
+    });
 };
 
 const thanks = () => {
-  const el = $('<div/>').addClass('thanks');
-  $body.prepend(el);
-
-  setTimeout(() => $('.thanks').addClass('grow'), 10);
-  setTimeout(() => $('.thanks').removeClass('grow'), 5000);
-  setTimeout(() => $('.thanks').remove(), 10000);
+  $('.thanks').addClass('grow');
+  setTimeout(() => $('.thanks').removeClass('grow'), 8000);
 };
 
 export const randomFeedbackAnimation = () => {
