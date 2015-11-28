7bdf2b06823d45a38ef2b8c2fa5e15e5c4aa894b
Simon Johansson <mail@simon-johansson.com>
2015-11-28 16:52:08 +0100

    Add some more feedback animations

--- public/js/feedback-animations.js
+++ public/js/feedback-animations.js
@@ -2,36 +2,41 @@
 import $ from 'jquery';
 import {random} from 'lodash';
 
-const $body = $('body');
-let counter = random(0, 2);
-
-const brad = () => {
-  $('.brad')
-    .addClass('drop')
+const animate = ($el, className) => {
+  $el
+    .addClass(className)
     .one('webkitTransitionEnd otransitionend oTransitionEnd transitionend',
       function (e) {
-        console.log('remove');
-        $(this).removeClass('drop');
+        $(this).removeClass(className);
     });
 };
 
-const walking = () => {
-  $('.walking')
-    .addClass('walk')
-    .one('webkitTransitionEnd otransitionend oTransitionEnd transitionend',
-      function (e) {
-        console.log('remove');
-        $(this).removeClass('walk');
-    });
-};
+const brad = () => animate($('.brad'), 'drop');
+const asta = () => animate($('.asta'), 'drop');
+const walking = () => animate($('.bean'), 'walking');
 
 const thanks = () => {
   $('.thanks').addClass('grow');
-  setTimeout(() => $('.thanks').removeClass('grow'), 8000);
+  setTimeout(() => $('.thanks').removeClass('grow'), 7000);
 };
 
+const thankyous = () => {
+  for (let ii = 0; ii <= 6; ii++) {
+    (function () {
+      const $el = $(`.cheesy${ii + 1}`);
+      setTimeout(() => {
+        $el.addClass('show');
+      }, random(700, 1000) * ii);
+      setTimeout(() => $el.removeClass('show'), 8000);
+    })();
+  };
+};
+
+const animations = [brad, walking, asta, thanks, thankyous];
+let counter = random(0, (animations.length - 1));
+
 export const randomFeedbackAnimation = () => {
-  const animations = [brad, walking, thanks];
-  animations[counter]();
+  // animations[counter]();
+  thanks();
   counter = counter >= (animations.length - 1) ? 0 : counter + 1;
 };
